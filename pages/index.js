339dbba61570b009387b297e3dc6ab2845b214f4
Faisaditya <faisadityaperdana@gmail.com>
2022-07-13 19:51:10 +0700

    Code Banner Product🧑🏻‍💻

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Product, HeroBanner, FooterBanner } from "../components";
+import { client } from "../lib/client";
 
-const index = () => {
+const Home = ({ products, bannerData }) => {
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+        <h1>Best Selling Product</h1>
+        <p>Speaker of many Variations</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
+      <FooterBanner />
+    </div>
+  );
+};
 
-export default index
+//mengambil data dari sanity
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
